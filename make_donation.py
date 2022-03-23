b8de36dd5a322b9a82810d674d1e29a3db8bfc26
Ubuntu John <johndavisdigitalart@gmail.com>
2022-03-23 13:40:22 -0700

    A few style fixes

--- make_donation.py
+++ make_donation.py
@@ -6,18 +6,19 @@ from decimal import *
 import sys
  
 merchantAuth = apicontractsv1.merchantAuthenticationType()
+# paste in your own Authorize.net username and transaction key
 merchantAuth.name = '...'
 merchantAuth.transactionKey = '...'
  
 creditCard = apicontractsv1.creditCardType()
-creditCard.cardNumber ="4111111111111111"
-creditCard.expirationDate ="2022-12"
+creditCard.cardNumber = '4111111111111111'
+creditCard.expirationDate = '2022-12'
  
 payment = apicontractsv1.paymentType()
 payment.creditCard = creditCard
  
 transactionrequest = apicontractsv1.transactionRequestType()
-transactionrequest.transactionType ="authCaptureTransaction"
+transactionrequest.transactionType = 'authCaptureTransaction'
 try:
        transactionrequest.amount = Decimal(sys.argv[1])
        print('Amount of money passed to Python: ' + sys.argv[1])
@@ -28,7 +29,7 @@ transactionrequest.payment = payment
  
 createtransactionrequest = apicontractsv1.createTransactionRequest()
 createtransactionrequest.merchantAuthentication = merchantAuth
-createtransactionrequest.refId ="MerchantID-0001"
+createtransactionrequest.refId = 'MerchantID-0001'
  
 createtransactionrequest.transactionRequest = transactionrequest
 createtransactioncontroller = createTransactionController(createtransactionrequest)
@@ -36,7 +37,7 @@ createtransactioncontroller.execute()
  
 response = createtransactioncontroller.getresponse()
  
-if (response.messages.resultCode=="Ok"):
+if (response.messages.resultCode == 'Ok'):
        print('Transaction ID: %s' % response.transactionResponse.transId)
 else:
        print('response code: %s' % response.messages.resultCode)
